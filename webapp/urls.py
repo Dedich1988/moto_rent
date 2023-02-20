6f83a9d8c00ed69f906252257b6beabadabb28b1
Dedich1988 <dedichpgu@gmail.com>
2023-02-20 22:18:33 +0300

    Finished the first video

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from webapp.views import *
 
+app_name = 'webapp'
+
 urlpatterns = [path('', index, name='home'),
                path('about/', about, name='about'),
                path('cars/', cars, name='cars'),
