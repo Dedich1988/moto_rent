bbb987ced816850a9fd63b7b067f4d539ece7034
Dedich1988 <dedichpgu@gmail.com>
2023-03-06 12:32:30 +0300

    Mosel car and paginator. troubles

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.safestring import mark_safe
+
 
 # Create your models here.
+class Car(models.Model):
+    """Car model"""
+    TRANSMISSION_CHOISE = [
+        ('automatic', 'Automatic'),
+        ('manual', 'Manual'),
+    ]
+    title = models.CharField(max_length=100)
+    door_num = models.PositiveIntegerField()
+    seat_num = models.PositiveIntegerField()
+    transmission = models.CharField(max_length=10, choices=TRANSMISSION_CHOISE)
+    rating = models.IntegerField(validators=[
+        MinValueValidator(0),
+        MaxValueValidator(5),
+    ])
+    price = models.PositiveIntegerField()
+    photo = models.ImageField(upload_to='car_photos/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name_plural = "Cars"
+
+    @property
+    def photo_url(self):
+        return self.photo.url if self.photo else ''
+
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="150" height="150" />' % self.photo.url)
+
+    image_tag.short_description = 'Image'
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from webapp.models import Car
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
@@ -8,7 +10,14 @@ def about(request):
     return render(request, 'webapp/about.html')
 
 def cars(request):
-    return render(request, 'webapp/cars.html')
+    """View for getting paginated cars"""
+
+    cars = Car.objects.all().order_by('price')
+    paginator = Paginator(cars, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'cars': page_obj}
+    return render(request, 'webapp/cars.html', context=context)
 
 def contact(request):
     return render(request, 'webapp/contact.html')
